0e87e73c9be6bdc8f8dde9dd85b2d2c59e7c1fa6
Kurxx33 <168380676+artemaslol@users.noreply.github.com>
2024-07-21 01:35:22 -0500

    Update database.php

--- docs/database.php
+++ docs/database.php
@@ -1,12 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "artemas";
+require 'vendor/autoload.php'; // Load the Composer autoload file
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+use MongoDB\Client;
+use MongoDB\Driver\ServerApi;
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Connection string for MongoDB Atlas
+$uri = 'mongodb+srv://kurxx33:<password>@cluster0artemas.hvdhn1l.mongodb.net/artemas?retryWrites=true&w=majority&appName=Cluster0artemas';
+
+// Set the version of the Stable API on the client
+$apiVersion = new ServerApi(ServerApi::V1);
+
+// Create a new client and connect to the server
+$client = new Client($uri, [], ['serverApi' => $apiVersion]);
+
+try {
+    // Send a ping to confirm a successful connection
+    $client->selectDatabase('admin')->command(['ping' => 1]);
+    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
+} catch (Exception $e) {
+    printf($e->getMessage());
+    exit();
 }
 ?>

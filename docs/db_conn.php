3429d2c95033e93412b709f42b2878f7b0aa2e40
Kurxx33 <168380676+Kurxx33@users.noreply.github.com>
2024-07-24 23:10:55 -0500

    Update db_conn.php

--- docs/db_conn.php
+++ docs/db_conn.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "sql312.infinityfree.com";
-$username = "if0_36910809"; // Your database username
-$password = "8uiC2xNW0z63xJf"; // Your database password
-$dbname = "if0_36910809_artemas_github"; // Your database name
+$username = "if0_36910809";
+$password = "8uiC2xNW0z63xJf";
+$dbname = "if0_36910809_artemas_github";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);

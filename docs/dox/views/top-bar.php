089519ad7615df5dfcc422bc9e634ebe6f2266e1
Kurxx33 <168380676+Kurxx33@users.noreply.github.com>
2024-07-28 05:46:21 -0500

    Create top-bar.php

---
+++ docs/dox/views/top-bar.php
@@ -0,0 +1,6 @@
+<div class="top-bar">
+    <a href="index.php?route=home">Home</a>
+    <a href="index.php?route=add_paste">Add Paste</a>
+    <a href="index.php?route=hall_of_losers">Hall of Losers</a>
+    <a href="index.php?route=history">History</a>
+</div>

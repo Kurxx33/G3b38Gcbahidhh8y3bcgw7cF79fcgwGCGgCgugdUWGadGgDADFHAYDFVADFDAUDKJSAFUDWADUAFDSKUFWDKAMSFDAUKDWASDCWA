d8c4f61355aba96b2452f6c946a1580e25a0c615
Kurxx33 <168380676+Kurxx33@users.noreply.github.com>
2024-08-28 11:32:49 -0500

    Update and rename top-bar.php to navbar.html

--- docs/dox/views/top-bar.php
+++
@@ -1,6 +0,0 @@
-<div class="top-bar">
-    <a href="index.php?route=home">Home</a>
-    <a href="index.php?route=add_paste">Add Paste</a>
-    <a href="index.php?route=hall_of_losers">Hall of Losers</a>
-    <a href="index.php?route=history">History</a>
-</div>

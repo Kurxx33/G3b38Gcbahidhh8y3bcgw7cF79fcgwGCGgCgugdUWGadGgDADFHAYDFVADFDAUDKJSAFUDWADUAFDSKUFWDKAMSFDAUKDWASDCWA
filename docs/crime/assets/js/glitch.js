c2796cf61b445942ce34b766b45bb834ef48903b
Kurxx33 <168380676+Kurxx33@users.noreply.github.com>
2024-09-05 12:39:44 -0500

    Create glitch.js

---
+++ docs/crime/assets/js/glitch.js
@@ -0,0 +1,27 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+let interval = null;
+
+document.querySelector("#evil").onmouseover = event => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.getAttribute('data-value')[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.getAttribute('data-value').length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 30);
+}
